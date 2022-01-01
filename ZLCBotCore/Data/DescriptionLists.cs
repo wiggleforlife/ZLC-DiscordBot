2fdfaea2347517678014ea0f6b1faa0f184426f8
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-01-01 14:04:17 -0700

    Modified Logic for geting description

--- ZLCBotCore/Data/DescriptionLists.cs
+++ ZLCBotCore/Data/DescriptionLists.cs
@@ -1,32 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
 using System.Text;
 
 namespace ZLCBotCore.Data
 {
     public static class DescriptionLists
     {
-        private static int ChanceToGetDescription = 25;
+        private static int CurrentIndex = -1;
+        private static DateTime LastUpdated = DateTime.UtcNow;
 
-        private static List<string> offlineDescriptionOptions = new List<string>
-        {
-            "Hey... Hey you... Yeah you... \nMaybe you should get online and control for a little bit?",
-            "Hey... You are already in the discord, why not control for a bit?",
-            "'Break for Control.'\n'Salt Lake Local, Stockton. Information.'\n...\nOh, wait no one is online. Sad Day.",
-            "Funny story: No one is online. \nOh wait thats not really that funny, is it?\nHmm, well you could always get online and control.",
-            "The 'Colonel' commands you to get online and control.",
-            "ZLC is a 24/7 ARTCC, well not in the virtual world, but you should still get online and control some."
-        };
+        private static List<string> atcZeroNotes = ReadFromGithub("https://raw.githubusercontent.com/Nikolai558/ZLC-DiscordBot/main/ZLCBotCore/atcZeroNotes.txt");
 
         public static string ChooseDescription(bool alwaysChooseDescription = true)
         {
-            var random = new Random();
-            if (alwaysChooseDescription || random.Next(0, 100) < ChanceToGetDescription) 
+            if (DateTime.UtcNow.Subtract(LastUpdated).TotalHours >= 1)
             {
-                int index = random.Next(offlineDescriptionOptions.Count);
-                return offlineDescriptionOptions[index];
+                atcZeroNotes = ReadFromGithub("https://raw.githubusercontent.com/Nikolai558/ZLC-DiscordBot/main/ZLCBotCore/atcZeroNotes.txt");
             }
-            return "\u200B";
+
+            if (CurrentIndex + 1 > atcZeroNotes.Count)
+            {
+                CurrentIndex = 0;
+            }
+            
+            CurrentIndex += 1;
+            string output = atcZeroNotes[CurrentIndex];
+            return output;
+        }
+
+        private static List<string> ReadFromGithub(string url)
+        {
+            using (WebClient webClient = new WebClient()) // TODO - Should this really be inside a using statement?
+            {
+                string text = webClient.DownloadString(url);
+
+                return text.Split('|').ToList();
+            }
         }
     }
 }
