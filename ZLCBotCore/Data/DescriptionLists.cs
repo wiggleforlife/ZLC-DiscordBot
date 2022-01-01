726df47565c4ab711109d7cbcdbfdf3f7e65fdab
Nikolia558 <38259407+Nikolai558@users.noreply.github.com>
2022-01-01 14:42:55 -0700

    Description Logic

--- ZLCBotCore/Data/DescriptionLists.cs
+++ ZLCBotCore/Data/DescriptionLists.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using ZLCBotCore.Models.AtcZeroNotesJson;
 
 namespace ZLCBotCore.Data
 {
@@ -12,32 +14,35 @@ namespace ZLCBotCore.Data
         private static int CurrentIndex = -1;
         private static DateTime LastUpdated = DateTime.UtcNow;
 
-        private static List<string> atcZeroNotes = ReadFromGithub("https://raw.githubusercontent.com/Nikolai558/ZLC-DiscordBot/main/ZLCBotCore/atcZeroNotes.txt");
+        private static atcZeroNotesJson atcZeroNotes = JsonConvert.DeserializeObject<atcZeroNotesJson>(ReadFromGithub("https://raw.githubusercontent.com/Nikolai558/ZLC-DiscordBot/main/ZLCBotCore/atcZeroNotes.txt"));
 
         public static string ChooseDescription(bool alwaysChooseDescription = true)
         {
             if (DateTime.UtcNow.Subtract(LastUpdated).TotalHours >= 1)
             {
-                atcZeroNotes = ReadFromGithub("https://raw.githubusercontent.com/Nikolai558/ZLC-DiscordBot/main/ZLCBotCore/atcZeroNotes.txt");
+                ReadFromGithub("https://raw.githubusercontent.com/Nikolai558/ZLC-DiscordBot/main/ZLCBotCore/atcZeroNotes.txt");
+                LastUpdated = DateTime.UtcNow;
             }
 
-            if (CurrentIndex + 1 > atcZeroNotes.Count)
+            if (CurrentIndex + 1 > atcZeroNotes.atcZeroNotes.Count())
             {
-                CurrentIndex = 0;
+                CurrentIndex = -1;
             }
             
             CurrentIndex += 1;
-            string output = atcZeroNotes[CurrentIndex];
+            string output = atcZeroNotes.atcZeroNotes[CurrentIndex];
+
+            //output.Replace("\\\\", "\\");
             return output;
         }
 
-        private static List<string> ReadFromGithub(string url)
+        private static string ReadFromGithub(string url)
         {
             using (WebClient webClient = new WebClient()) // TODO - Should this really be inside a using statement?
             {
-                string text = webClient.DownloadString(url);
+                var json = webClient.DownloadString(url);
 
-                return text.Split('|').ToList();
+                return json;
             }
         }
     }
---
+++ ZLCBotCore/Models/AtcZeroNotesJson/atcZeroNotesJson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ZLCBotCore.Models.AtcZeroNotesJson
+{
+    public class atcZeroNotesJson
+    {
+        public string[] atcZeroNotes { get; set; }
+    }
+
+}
